78a6641f899df8452db5bd90413b9f81d4d503a3
Omoboi-coder <bukunmiabiodun14@gmail.com>
2025-08-13 13:01:02 +0100

    nineth commit

--- src/app/home/Component/RightHomeRow3.js
+++ src/app/home/Component/RightHomeRow3.js
@@ -7,139 +7,157 @@ const RightHomeRow3 = () => {
        {/* Main container Row 3 */}
        <div className='px-4 md:px-0'>
         <div className='flex flex-row w-full md:w-[1065px] h-[600px] rounded-[12px] bg-[#00004C] 
-        mt-5 gap-20 justify-center md:justify-start'>
+        mt-5 gap-4 md:gap-20 justify-center md:justify-start p-4 md:p-0'>
           {/* Left item */}
-          <div className='flex flex-col w-[240px] min-h-full md:h-[670px] mt-3'>
-              <h4 className='text-[16px] text-white pt-2 pl-4'>
+          <div className='flex flex-col w-full md:w-[240px] min-h-full md:h-[670px] mt-3'>
+              <h4 className='text-[20px] md:text-[16px] text-white pt-2 pl-4 md:pl-4'>
                 Investments
               </h4>
-              <div className='h-[600px] space-y-5 p-4'>
-                  <div className='flex items-center w-[232px] h-[120px] rounded-[16px] bg-[#FFFFFF]'>
-                    <div className='flex relative w-[193px] h-[83px] mx-auto gap-[48px]'>
-                      <div className='w-[137px] h-[83px]'>
-                        <div className='flex flex-row gap-[14px]'>
+              <div className='h-auto md:h-[600px] space-y-4 md:space-y-5 p-2 md:p-4'>
+                  {/* Investment Card 1 */}
+                  <div className='flex items-center w-full max-w-[320px] md:w-[232px] h-[120px] 
+                  mx-auto md:mx-0 rounded-[16px] bg-[#FFFFFF]'>
+                    <div className='flex relative w-full max-w-[280px] md:w-[193px] h-[83px] mx-auto px-4 md:px-0'>
+                      <div className='flex-1 md:w-[137px] h-[83px]'>
+                        <div className='flex flex-row gap-[10px] md:gap-[14px] items-center'>
                           <Image
                           src="/Images/cryptocurrency-color_etc.svg"
                           alt="Ethereum Classic Logo"
                           width="40"
                           height="40"
-                          className='w-[40px] h-[40px]'
+                          className='w-[50px] h-[50px] md:w-[40px] md:h-[40px] flex-shrink-0'
                           />
-                          <p className='text-[#010101] text-[20px] pt-3'>
+                          <p className='text-[#010101] text-[25px] md:text-[20px] font-medium'>
                             $20,000
                           </p>
                         </div>
-                        <Image
-                        src="/Images/Frame 140 (2).svg"
-                        alt=''
-                        width="88"
-                        height="15"
-                        className='w-[88px] h-[15px] mt-6'
-                        />
-                      </div>
-                      <Image
-                        src="/Images/option.svg"
-                        alt=''
-                        width="8"
-                        height="23"
-                        className='absolute bottom-[3.6rem] left-46 w-[8px] h-[23px]'
-                        />
-                    </div>
-                  </div>
-                     <div className='flex items-center w-[232px] h-[120px] rounded-[16px] bg-[#FFFFFF]'>
-                    <div className='flex items-center relative w-[193px] h-[83px] ml-5 gap-[48px]'>
-                      <div className='w-[137px] h-[83px]'>
-                        <div className='flex flex-row gap-[14px]'>
+                        <div className='mt-3 md:mt-6'>
                           <Image
-                          src="/Images/cryptocurrency-color_matic.svg"
-                          alt="Ethereum Classic Logo"
-                          width="40"
-                          height="40"
-                          className='w-[40px] h-[40px]'
+                          src="/Images/Frame 140 (2).svg"
+                          alt=''
+                          width="88"
+                          height="15"
+                          className='w-[95px] md:w-[88px] h-[12px] md:h-[15px]'
                           />
-                          <p className='text-[#010101] text-[20px] pt-3'>
-                            $18,000
-                          </p>
                         </div>
-                        <Image
-                        src="/Images/Frame 140 (1).svg"
-                        alt=''
-                        width="88"
-                        height="15"
-                        className='w-[88px] h-[15px] mt-6'
-                        />
                       </div>
                       <Image
                         src="/Images/option.svg"
                         alt=''
                         width="8"
                         height="23"
-                        className='absolute bottom-[3.6rem] left-46 w-[8px] h-[23px]'
-                        />
-                    </div>
-                  </div>
-                     <div className='flex items-center w-[232px] h-[120px] rounded-[16px] bg-[#FFFFFF]'>
-                    <div className='flex items-center relative w-[193px] h-[83px] ml-5 gap-[48px]'>
-                      <div className='w-[137px] h-[83px]'>
-                        <div className='flex flex-row gap-[14px]'>
-                          <Image
-                          src="/Images/Group (1).svg"
-                          alt="Ethereum Classic Logo"
-                          width="40"
-                          height="40"
-                          className='w-[40px] h-[40px]'
-                          />
-                          <p className='text-[#010101] text-[20px] pt-3'>
-                            $12,000
-                          </p>
-                        </div>
-                        <Image
-                        src="/Images/Frame 140 (1).svg"
-                        alt=''
-                        width="88"
-                        height="15"
-                        className='w-[88px] h-[15px] mt-6'
-                        />
-                      </div>
-                      <Image
-                        src="/Images/option.svg"
-                        alt=''
-                        width="8"
-                        height="23"
-                        className='absolute bottom-[3.6rem] left-46 w-[8px] h-[23px]'
+                        className='absolute top-2 right-2 md:bottom-[3.6rem] md:left-46 w-[8px] h-[23px]'
                         />
                     </div>
                   </div>
 
-                     <div className='flex items-center w-[232px] h-[120px] rounded-[16px] bg-[#FFFFFF]'>
-                    <div className='flex items-center relative w-[193px] h-[83px] ml-5 gap-[48px]'>
-                      <div className='w-[137px] h-[83px]'>
-                        <div className='flex flex-row gap-[14px]'>
+                  {/* Investment Card 2 */}
+                  <div className='flex items-center w-full max-w-[320px] md:w-[232px] h-[120px] mx-auto
+                   md:mx-0 rounded-[16px] bg-[#FFFFFF]'>
+                    <div className='flex relative w-full max-w-[280px] md:w-[193px] h-[83px] mx-auto px-4 md:px-0'>
+                      <div className='flex-1 md:w-[137px] h-[83px]'>
+                        <div className='flex flex-row gap-[10px] md:gap-[14px] items-center'>
                           <Image
-                          src="/Images/cryptocurrency-color_bnb.svg"
-                          alt="Ethereum Classic Logo"
+                          src="/Images/cryptocurrency-color_matic.svg"
+                          alt="Polygon MATIC Logo"
                           width="40"
                           height="40"
-                          className='w-[40px] h-[40px]'
+                          className='w-[50px] h-[50px] md:w-[40px] md:h-[40px] flex-shrink-0'
                           />
-                          <p className='text-[#010101] text-[20px] pt-3'>
-                            $600
+                          <p className='text-[#010101] text-[25px] md:text-[20px] font-medium'>
+                            $18,000
                           </p>
                         </div>
-                        <Image
-                        src="/Images/Frame 140 (1).svg"
-                        alt=''
-                        width="88"
-                        height="15"
-                        className='w-[88px] h-[15px] mt-6'
-                        />
+                        <div className='mt-3 md:mt-6'>
+                          <Image
+                          src="/Images/Frame 140 (1).svg"
+                          alt=''
+                          width="88"
+                          height="15"
+                          className='w-[95px] md:w-[88px] h-[20px] md:h-[15px]'
+                          />
+                        </div>
                       </div>
                       <Image
                         src="/Images/option.svg"
                         alt=''
                         width="8"
                         height="23"
-                        className='absolute bottom-[3.6rem] left-46 w-[8px] h-[23px]'
+                        className='absolute top-2 right-2 md:bottom-[3.6rem] md:left-46 w-[8px] h-[23px]'
+                        />
+                    </div>
+                  </div>
+
+                  {/* Investment Card 3 */}
+                  <div className='flex items-center w-full max-w-[320px] md:w-[232px] h-[120px] mx-auto 
+                  md:mx-0 rounded-[16px] bg-[#FFFFFF]'>
+                    <div className='flex relative w-full max-w-[280px] md:w-[193px] h-[83px] mx-auto px-4 md:px-0'>
+                      <div className='flex-1 md:w-[137px] h-[83px]'>
+                        <div className='flex flex-row gap-[10px] md:gap-[14px] items-center'>
+                          <Image
+                          src="/Images/Group (1).svg"
+                          alt="Investment Logo"
+                          width="40"
+                          height="40"
+                          className='w-[50px] h-[50px] md:w-[40px] md:h-[40px] '
+                          />
+                          <p className='text-[#010101] text-[25px] md:text-[20px] font-medium'>
+                            $12,000
+                          </p>
+                        </div>
+                        <div className='mt-3 md:mt-6'>
+                          <Image
+                          src="/Images/Frame 140 (1).svg"
+                          alt=''
+                          width="88"
+                          height="15"
+                          className='w-[95px] md:w-[88px] h-[20px] md:h-[15px]'
+                          />
+                        </div>
+                      </div>
+                      <Image
+                        src="/Images/option.svg"
+                        alt=''
+                        width="8"
+                        height="23"
+                        className='absolute top-2 right-2 md:bottom-[3.6rem] md:left-46 w-[8px] h-[23px]'
+                        />
+                    </div>
+                  </div>
+
+                  {/* Investment Card 4 */}
+                  <div className='flex items-center w-full max-w-[320px] md:w-[232px] h-[120px] mx-auto 
+                  md:mx-0 rounded-[16px] bg-[#FFFFFF]'>
+                    <div className='flex relative w-full max-w-[280px] md:w-[193px] h-[83px] mx-auto px-4 md:px-0'>
+                      <div className='flex-1 md:w-[137px] h-[83px]'>
+                        <div className='flex flex-row gap-[10px] md:gap-[14px] items-center'>
+                          <Image
+                          src="/Images/cryptocurrency-color_bnb.svg"
+                          alt="BNB Logo"
+                          width="40"
+                          height="40"
+                          className='w-[50px] h-[50px] md:w-[40px] md:h-[40px] flex-shrink-0'
+                          />
+                          <p className='text-[#010101] text-[25px] md:text-[20px] font-medium'>
+                            $600
+                          </p>
+                        </div>
+                        <div className='mt-3 md:mt-6'>
+                          <Image
+                          src="/Images/Frame 140 (1).svg"
+                          alt=''
+                          width="88"
+                          height="15"
+                          className='w-[95px] md:w-[88px] h-[20px] md:h-[15px]'
+                          />
+                        </div>
+                      </div>
+                      <Image
+                        src="/Images/option.svg"
+                        alt=''
+                        width="8"
+                        height="23"
+                        className='absolute top-2 right-2 md:bottom-[3.6rem] md:left-46 w-[8px] h-[23px]'
                         />
                     </div>
                   </div>
@@ -148,7 +166,7 @@ const RightHomeRow3 = () => {
 
           {/* Right item */}
           <div className='hidden md:flex flex-col'>
-            <div className='flex flex-row justify-between  w-[686px] h-[29px] mt-4 items-end'>
+            <div className='flex flex-row justify-between w-[686px] h-[29px] mt-4 items-end'>
               <h4 className='text-white'>
                 Investment Growth
               </h4>
